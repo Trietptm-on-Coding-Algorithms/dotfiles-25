68c86e20cdff212d3aa9498cde142b00e44c8139
Cameron Bamford <cameron@dbamford.co.uk>
2017-12-31 08:47:15 +0000

    Added xorg keyboard conf, pacman conf, reduced vim to just basic vimrc, wrote install script.

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [[ ":$PWD:" == *"dotfiles"* ]];
+then
+    stow --verbose=2 awesome
+    stow --verbose=2 rofi
+    stow --verbose=2 vim
+    stow --verbose=2 zsh
+    stow --verbose=2 xorg
+
+    sudo stow --verbose=2 pacman -t /
+    sudo stow --verbose=2 keyboard -t /
+else
+    echo "ERROR: CD into the dotfiles dir."
+    exit 1
+fi
