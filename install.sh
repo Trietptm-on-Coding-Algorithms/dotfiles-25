53e8596aa341730bc31dc77ec332fab7de978182
Cameron Bamford <cameron@dbamford.co.uk>
2018-01-01 18:15:53 +0000

    Added custom i3lock-color script

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ then
     stow --verbose=2 gtk3
 
     sudo stow --verbose=2 pacman -t /
+    sudo stow --verbose=2 bin -t /
     sudo stow --verbose=2 keyboard -t /
 else
     echo "ERROR: CD into the dotfiles dir."
